f8dc776cf9519b80f84c80a187bcc676a36836d1
Diego Yon <diegoyon@outlook.com>
2022-09-14 09:19:44 -0600

    Create vets, specializations and visits tables

--- schema.sql
+++ schema.sql
@@ -40,3 +40,11 @@ ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES
 -- Add column owner_id which is a foreign key referencing the owners table
 ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_owner FOREIGN KEY(owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR,
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY (id),
+);
